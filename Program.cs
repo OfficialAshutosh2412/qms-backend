9a6a28a102754b072d3078599251d4869362f747
Ashutosh Prasad <ashutoshprasad2427@gmail.com>
2026-03-21 13:19:30 +0530

    added live react app url in cors allowed

--- Program.cs
+++ Program.cs
@@ -121,7 +121,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactApp", policy =>
     {
-        policy.WithOrigins("http://localhost:5173").AllowAnyHeader().AllowAnyMethod().AllowCredentials();
+        policy.WithOrigins("http://localhost:5173", "https://qms-frontend-lovat.vercel.app").AllowAnyHeader().AllowAnyMethod().AllowCredentials();
     });
 });
 
